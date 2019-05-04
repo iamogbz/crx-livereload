707ed73e0d5c4bb4691cfce6bfb4e3a11b789a06
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2019-05-04 10:25:10 -0400

    chore: point upstream to node-js-boilerplate

--- .github/scripts/configure.sh
+++ .github/scripts/configure.sh
@@ -3,4 +3,5 @@
 git clone https://github.com/iamogbz/node-js-boilerplate ../$REPO_NAME
 cd ../$REPO_NAME
 git remote set-url origin $REPO_URL
+sed -i '' 's/oss-boilerplate/node-js-boilerplate/g' Makefile
 make upstream
