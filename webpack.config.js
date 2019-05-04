22b06781d52d3eb60f5c491f8d47c038c45b8df2
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2019-05-04 15:48:34 -0400

    chore: reorder webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,14 @@
 const path = require("path");
 
 module.exports = {
-    mode: "production",
+    devtool: "source-map",
     entry: "./src",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "lib"),
-        libraryTarget: "commonjs-module",
-    },
+    mode: "production",
     module: {
         rules: [
             {
-                test: /\.jsx?$/,
                 exclude: /(node_modules|bower_components)/,
+                test: /\.jsx?$/,
                 use: {
                     loader: "babel-loader",
                     options: {
@@ -23,5 +19,13 @@ module.exports = {
             },
         ],
     },
-    devtool: "source-map",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "lib"),
+        libraryTarget: "commonjs-module",
+    },
+    resolve: {
+        extensions: [".js", ".jsx"],
+        modules: [path.resolve("./src"), path.resolve("./node_modules")],
+    },
 };
