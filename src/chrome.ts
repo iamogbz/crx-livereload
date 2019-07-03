775c9f7f3cc6d33b08b4880fafcaf026fa96ed41
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2019-07-03 08:36:06 -0400

    feat: add logging to background

--- src/chrome.ts
+++ src/chrome.ts
@@ -1,7 +1,9 @@
-export const getExtensionInfo = () =>
+export const log = console.log.bind(console); // tslint:disable-line no-console
+
+export const getExtensionInfo = (): Promise<chrome.management.ExtensionInfo> =>
     new Promise(resolve => chrome.management.getSelf(resolve));
 
-export const getExtensionDirectory = () =>
+export const getExtensionDirectory = (): Promise<DirectoryEntry> =>
     new Promise(resolve => chrome.runtime.getPackageDirectoryEntry(resolve));
 
 export const getEntries = (dir: DirectoryEntry): Promise<Entry[]> =>
@@ -11,6 +13,10 @@ export const reloadExtensionTab = () =>
     new Promise(resolve =>
         chrome.tabs.query({ active: true, currentWindow: true }, resolve),
     ).then(([tab]) => {
+        log("🔥 Reloading extension");
         chrome.runtime.reload();
-        if (tab) chrome.tabs.reload(tab.id);
+        if (tab) {
+            log("🔥 Reloading browser tab");
+            chrome.tabs.reload(tab.id);
+        }
     });
